f47544f946d560cfdc769d01627e69d96f44bb94
Ganenco Dmitri <ganenko.dmitri@gmail.com>
2020-08-04 11:20:33 +0300

    Summary

--- js/hw-02/hw-02-6.js
+++ js/hw-02/hw-02-6.js
@@ -13,6 +13,7 @@ function mapArray(array) {
   const numbers = new Array(array.length);
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
+    numbers[i] = array[i] * 10;
   }
   return numbers;
 }
